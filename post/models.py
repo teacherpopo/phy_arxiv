7e6de6c5ab605365043bfa7c7a97b1767f60a3b0
teacherpopo <yushen0612@gmail.com>
2016-07-11 13:13:36 -0400

    Added a comment model

--- post/models.py
+++ post/models.py
@@ -17,9 +17,26 @@ class PostModel(models.Model):
 
 	#A "draft" turns into a "post" after submission 
 	status = models.CharField(max_length=20)
+
+	#"private" or "public", default is "private".
+	scope = models.CharField(max_length=20)
 	
 	owner = models.ForeignKey(User, related_name='owner')
 	authors = models.ManyToManyField(User, related_name='authors')
 
 	def __unicode__(self):
 		return self.title
+
+class CommentModel(models.Model):
+	date_stamp = models.DateTimeField()
+	content = models.TextField()
+
+	#"private" or "public", default is "private".
+	scope = models.CharField(max_length=20)
+
+	#The parent post a comment belongs to.
+	parent_post = models.ForeignKey(PostModel, related_name='comments')
+
+	commenter = models.ForeignKey(User, related_name='commenter')
+	#A commenter can specify a user to reply to.
+	reply_to = models.ForeignKey(User, null=True, blank=True, related_name = 'reply_to')
